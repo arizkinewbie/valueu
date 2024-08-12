600dfe56293b07583b5cf8ac2973b1d30b17b40f
arizkinewbie <arizkijitsu@gmail.com>
2024-08-12 09:41:57 +0700

    add initial api

---
+++ app/Controllers/Api.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\HomeModel;
+use App\Models\TokenModel;
+use App\Models\DashboardModel;
+
+class Api extends BaseController
+{
+	protected $model;
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->model = new HomeModel;
+	}
+
+	public function check()
+	{
+		$token = new TokenModel();
+		try {
+			$tokenData = $_GET['token'];
+			$dataToken = $token->verifyToken($tokenData);
+		} catch (\Exception $e) {
+			$dataToken = [
+				'status' => 500,
+				'error' => $e->getMessage(),
+				'message' => 'Terjadi kesalahan! data tidak tersedia',
+				'data' => []
+			];
+		}
+		return $this->response->setJSON($dataToken);
+	}
+
+	public function infoToken()
+	{
+		$model = new DashboardModel();
+		try {
+			$data = $model->getAllToken();
+			$data = [
+				'status' => 200,
+				'message' => 'Data tersedia',
+				'data' => $data
+			];
+		} catch (\Exception $e) {
+			$data = [
+				'status' => 500,
+				'error' => $e->getMessage(),
+				'message' => 'Terjadi kesalahan! data tidak tersedia',
+				'data' => []
+			];
+		}
+		return $this->response->setJSON($data);
+	}
+}
